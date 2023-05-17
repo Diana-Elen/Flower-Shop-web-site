7228360b4f2de4ccf7e8e2efb1b9d2a844200d8e
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-17 23:55:51 +0300

    update logo

--- flower-shop/components/Logo.js
+++ flower-shop/components/Logo.js
@@ -1,16 +1,15 @@
 import style from "../styles/Logo.module.css"
-import logo from "../images/logo.png"
+import logo from "../images/logo.jpg"
 import Image from "next/image";
 
 export default function Logo() {
-    
     return <>
     <Image
-          className={style.logo}
-          src={logo.src}
-          alt="logo image"
-          width={500}
-          height={300}
+        className={style.logo}
+        src={logo.src}
+        alt="logo image"
+        width={500}
+        height={300}
         />
     </>
 }
