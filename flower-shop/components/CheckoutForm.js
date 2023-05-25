45462070d04ebc19ee7d8a47703be75c1644d78c
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-25 21:27:06 +0300

    update form

--- flower-shop/components/CheckoutForm.js
+++ flower-shop/components/CheckoutForm.js
@@ -7,16 +7,15 @@ const CheckoutForm = () => {
  const confirmationScreenVisible =
    router.query?.success && router.query.success === "true";
  const formVisible = !confirmationScreenVisible;
- 
  const ConfirmationMessage = (
-   <React.Fragment>
+   <>
      <p>
        Thank you for submitting this form. Someone should get back to you
        within 24-48 hours.
      </p>
  
      <button onClick={() => router.replace("/contact", undefined, { shallow: true })}> Submit Another Response </button>
-   </React.Fragment>
+   </>
  );
  
  const ContactForm = (
@@ -61,7 +60,7 @@ const CheckoutForm = () => {
  return (
    <div>
      <h1>Contact Us</h1>
-{formVisible ? ContactForm : ConfirmationMessage}
+     {formVisible ? ContactForm : ConfirmationMessage}
    </div>
  );
 };
