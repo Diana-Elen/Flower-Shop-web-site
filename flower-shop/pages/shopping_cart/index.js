281fead4ae2092dbcd03aef0b250a80e97d0aff3
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-26 22:51:06 +0300

    add checkout form

--- flower-shop/pages/shopping_cart/index.js
+++ flower-shop/pages/shopping_cart/index.js
@@ -2,12 +2,14 @@ import MyCarousel from "@/components/MyCarousel"
 import Nav from "@/components/Nav"
 import TabNav from "@/components/TabNav"
 import Footer from "@/components/Footer"
+import CheckoutForm from "@/components/CheckoutForm"
 
 
 export default function ShoppingCart({cartItems}) {
     return <>
     <Nav />
     <MyCarousel />
+    <CheckoutForm />
     <Footer />
     </>
 }
