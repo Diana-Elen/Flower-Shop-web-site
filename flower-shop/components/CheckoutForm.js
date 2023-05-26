b462d6bba322d54669dd568fda19eee685ab1ff8
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-26 22:37:09 +0300

    update form

--- flower-shop/components/CheckoutForm.js
+++ flower-shop/components/CheckoutForm.js
@@ -3,40 +3,40 @@ import style from "../styles/Form.module.css"
 export default function CheckoutForm() {
 
   return <>
-    <form className={style.contact_form} form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-      <input type="hidden" name="checkout-form" value="contact-form"/>
-        <div className={style.form_style}>
-        <h1 className={style.title}>Fill in the details of your order</h1>
-          <label className={style.form_group}>
-              Name
-              <input type="text" name="checkbox_name" placeholder="Name" required/>
-          </label>
-          <label className={style.form_group}>
-              Address
-              <input type="text" name="checkbox_address" placeholder="Address" required/>
-          </label>
-          <label class={style.form_group}>
-              Email
-          <input type="email" name="checkbox_email" placeholder="user@domain.ext" required/>
-          </label>
-          <label className={style.form_group}>
-              Message
-          <textarea placeholder="fill in your remarks" name="message" required>
-          </textarea>
-          </label>
-          <div className={style.checkbox_group}>
-              <input type="checkbox" name="checkbox_accept" required/>
-              <label for="val1">I read the terms and conditions</label>
-          </div>
-          <div className={style.btn_group}>
-              <button className={style.button_style} type="reset" value="Reset the form">
-                Reset the form
-              </button>
-              <button className={style.button_style} type="submit" value="Send order">
-                Send
-              </button>
-          </div>
+    <form className={style.contact_form} form name="my-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <input type="hidden" name="form-name" value="my-form"/>
+      <div className={style.form_style}>
+      <h1 className={style.title}>Fill in the details of your order</h1>
+        <label className={style.form_group}>
+            Name
+            <input type="text" name="checkbox_name" placeholder="Name" required/>
+        </label>
+        <label className={style.form_group}>
+            Address
+            <input type="text" name="checkbox_address" placeholder="Address" required/>
+        </label>
+        <label class={style.form_group}>
+            Email
+        <input type="email" name="checkbox_email" placeholder="user@domain.ext" required/>
+        </label>
+        <label className={style.form_group}>
+            Message
+        <textarea placeholder="fill in your remarks" name="message" required>
+        </textarea>
+        </label>
+        <div className={style.checkbox_group}>
+            <input type="checkbox" name="checkbox_accept" required/>
+            <label for="val1">I read the terms and conditions</label>
         </div>
+        <div className={style.btn_group}>
+            <button className={style.button_style} type="reset" value="Reset the form">
+              Reset the form
+            </button>
+            <button className={style.button_style} type="submit" value="Send order">
+              Send
+            </button>
+        </div>
+      </div>
       </form>
   </>
 }
