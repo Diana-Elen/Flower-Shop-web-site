9eb4d1b6ca575cde957ca30131d3a7afe48fa311
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-26 23:12:03 +0300

    add cart and productgroup

--- flower-shop/pages/index.js
+++ flower-shop/pages/index.js
@@ -2,14 +2,16 @@ import MyCarousel from "@/components/MyCarousel"
 import Nav from "../components/Nav"
 import TabNav from "@/components/TabNav"
 import Footer from "@/components/Footer"
-import App from "@/components/App"
+import Cart from "@/components/Cart"
+import ProductGroup from "@/components/ProductGroup"
 
 export default function Home() {
   return <>
     <Nav />
     <MyCarousel />
     <TabNav />
-    <App />
+    <ProductGroup />
+    <Cart />
     <Footer />
   </>
 }
