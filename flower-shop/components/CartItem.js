248af4c7105f6eef6766b72b2660f806527a55a7
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-28 00:18:55 +0300

    update cartitems css

--- flower-shop/components/CartItem.js
+++ flower-shop/components/CartItem.js
@@ -6,16 +6,16 @@ import { useContext } from "react";
 export default function CartItem ({product}) {
   const {addProducts, removeProducts, cartItems} = useContext(ShoppingCartContext);
   return <>
-    <div className={style.product_container}>
-    <div className={style.cart_product}>
-      <div className={style.name_price}>
-      <div className={style.product_name}></div>
-        <div className={style.product_name}>{product.name}</div>
-        <div className={style.product_price}>{product.price}$</div>
+    <div className={style.cartitems_container}>
+      <div className={style.cart_product}>
+        <div className={style.name_price}>
+        <div className={style.product_name}></div>
+          <div className={style.product_name}>{product.name}</div>
+          <div className={style.product_price}>{product.price}$</div>
+        </div>
+        <Image className={style.image_size} src={product.image} width={250} height={250} alt="image">
+        </Image>
       </div>
-      <Image className={style.image_size} src={product.image} width={250} height={250} alt="image">
-      </Image>
-    </div>
     </div>
   </>
 }
