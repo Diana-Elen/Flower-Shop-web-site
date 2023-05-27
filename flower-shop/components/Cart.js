27200978d7e356affc0bfcca8e5dfe2481535bf3
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-27 23:55:51 +0300

    update cartitem to appear in the cart

--- flower-shop/components/Cart.js
+++ flower-shop/components/Cart.js
@@ -13,14 +13,10 @@ export default function Cart ({products}) {
         <h1 className={style.text}>Your cart items</h1>
         {products.map((product) => {
           if (cartItems[product.id] !== 0) {
-            return 
-              <CartItem key={product.nr}>
-                id={product.id}
-                product={product} 
-                image={product.image}
-                name={product.name}
-                price={product.price}
-                </CartItem>
+            return <>
+              <CartItem key={product.nr}
+                product={product} />
+            </>
           }          
         })}
       </div>
@@ -28,9 +24,6 @@ export default function Cart ({products}) {
         <div>
           <div className={style.final_price}>
             <p className={style.total_pay}> Total: {totalAmount}$</p>
-            <button className={style.checkout_button}>
-              
-            </button>
             <CheckoutForm totalAmount={totalAmount}/>
           </div>
         </div>
--- flower-shop/components/CartItem.js
+++ flower-shop/components/CartItem.js
@@ -3,17 +3,17 @@ import Image from "next/image"
 import { ShoppingCartContext } from "@/context/ShoppingCartContext";
 import { useContext } from "react";
 
-export default function CartItem ({price, name, image, id}) {
+export default function CartItem ({product}) {
   const {addProducts, removeProducts, cartItems} = useContext(ShoppingCartContext);
   return <>
     <div className={style.product_container}>
     <div className={style.cart_product}>
       <div className={style.name_price}>
       <div className={style.product_name}></div>
-        <div className={style.product_name}>{name}</div>
-        <div className={style.product_price}>{price}$</div>
+        <div className={style.product_name}>{product.name}</div>
+        <div className={style.product_price}>{product.price}$</div>
       </div>
-      <Image className={style.image_size} src={image} width={250} height={250} alt="image">
+      <Image className={style.image_size} src={product.image} width={250} height={250} alt="image">
       </Image>
     </div>
     </div>
