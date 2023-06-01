a699af47f7173350969a9d9c4ebcdcc5d9d9b6e6
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-01 07:50:08 +0300

    added localStorage

--- flower-shop/context/ShoppingCartContext.js
+++ flower-shop/context/ShoppingCartContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import data from "@/components/data";
 import ProductGroup from "@/components/ProductGroup";
 
@@ -29,16 +29,26 @@ export const ShoppingCartContextProvider = (props) => {
 
     const addProducts = (itemID) => {
         setCartItems((prev) => ({...prev, [itemID]: prev[itemID] + 1}));
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
     }
 
     const removeProducts = (itemID) => {
         setCartItems((prev) => ({...prev, [itemID]: prev[itemID] - 1}));
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
     }
 
     const checkout = () => {
     setCartItems(getDefaultCartStatus());
     };
-     
+
+    useEffect(() => {
+      setCartItems(localStorage.getItem("cartItems")
+      ?
+       JSON.parse(localStorage.getItem("cartItems"))
+       : []
+      );
+    }, []);
+    
     const contextValue = {cartItems, addProducts, removeProducts, getTotalCartAmount, checkout};
 
     return <ShoppingCartContext.Provider value={contextValue}>
