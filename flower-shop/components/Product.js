573c2c46148567c238f4d27e48568821b6867c9d
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-11 12:59:50 +0300

    update product css

--- flower-shop/components/Product.js
+++ flower-shop/components/Product.js
@@ -10,20 +10,17 @@ export default function Product ({id, price, name, image}) {
   
     return <>
       <div className={style.product_container}>
-        <div className={style.prod}>
-          <div className={style.name_price}>
-          <div className={style.product_name}></div>
-            <div className={style.product_name}>{name}</div>
-            <div className={style.product_price}>{price}$</div>
-          </div>
-          <Image className={style.image_size} src={image} width={250} height={250} alt="image">
-          </Image>
-          <button onClick={() => (addProducts(id))} className={style.add_remove_button}>
-          <ShoppingCart size={32}/>
-          {cartItemsAmount > 0 && <>({cartItemsAmount})</>}
-          </button>   
-          
-        </div>         
+        <div className={style.name_price}>
+        <div className={style.product_name}></div>
+          <div className={style.product_name}>{name}</div>
+          <div className={style.product_price}>{price}$</div>
+        </div>
+        <Image className={style.image_size} src={image} width={250} height={250} alt="image">
+        </Image>
+        <button onClick={() => (addProducts(id))} className={style.add_remove_button}>
+        <ShoppingCart size={32}/>
+        {cartItemsAmount > 0 && <>({cartItemsAmount})</>}
+        </button>      
       </div>
     </>
 }
