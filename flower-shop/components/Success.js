629509558b8e923f373198166cf4e1d7a800e1dc
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-06-11 12:45:28 +0300

    delete success component

--- flower-shop/components/Success.js
+++
@@ -1,9 +0,0 @@
-import style from "../styles/Success.module.css"
-
-export default function Success() {
-    return <>
-    <div className={style.success_message}>
-        Thank you for your order
-    </div>
-    </>
-}
