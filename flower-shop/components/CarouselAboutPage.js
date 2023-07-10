e5282f5277095000acd772abad8ccdb07a25a26e
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-07-10 10:42:16 +0300

    update carousel

--- flower-shop/components/CarouselAboutPage.js
+++ flower-shop/components/CarouselAboutPage.js
@@ -1,24 +1,21 @@
 import { useState } from "react";
 import data from "./data";
 import Image from "next/image";
+import style from "../styles/CarouselAboutPage.module.css"
 
 export default function CarouselAboutPage() {
     const [index, setIndex] = useState(0);
-
-
     function handleClick() {
         setIndex(index + 1);
     }
-
     let myimages = data.products[index];
-
     return <>
-    <div>
-        <h2>Check out my favourite bouquets:</h2>
-        <button onClick={handleClick}>next</button>
-        <h3>{myimages.name}</h3>
+    <div className={style.carousel_container}>
+        <h2 className={style.carousel_title}>Check out my favourite bouquets:</h2>
+        <h4>{myimages.name}</h4>
         <Image src={myimages.image} height={300} width={300} />
-        <h3>{myimages.price}$</h3>
+        <h5>Price: {myimages.price}$</h5>
+        <button onClick={handleClick}>next</button>
     </div>
     </>
 }
